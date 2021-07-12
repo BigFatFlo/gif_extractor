ec58fc618da0bed4d2bf7f0b84c48ccec92ccf87
Florent Remis <florent.remis@gmail.com>
2021-07-12 10:34:32 +0200

    Showing start and end frames

--- gif_extractor.py
+++ gif_extractor.py
@@ -10,11 +10,10 @@ from threading import Thread, RLock
 import signal
 from PySide2.QtWidgets import QApplication, QMainWindow, QLabel
 from PySide2.QtCore import QFile, QThread, Signal, QObject, QRunnable, QThreadPool, QPoint, QRect, QByteArray, QTimer, Qt
-from PySide2.QtGui import QTextCursor, QIcon, QPixmap, QPainter, QPen, QColor, QPainterPath, QPainterPathStroker, QBrush
+from PySide2.QtGui import QTextCursor, QIcon, QPixmap, QImage, QPainter, QPen, QColor, QPainterPath, QPainterPathStroker, QBrush
 from ui_mainwindow import Ui_MainWindow
 from PySide2.QtWidgets import QStyleFactory;
 import struct
-import shlex
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -23,21 +22,19 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
 
-def launch_extraction(start_time_ms, end_time_ms, top_line_text, bottom_line_text, gif_name):
-    # Opens the Video file
-    cap = cv2.VideoCapture('./test_video.mp4')
+def launch_extraction(video, start_time_ms, end_time_ms, top_line_text, bottom_line_text, gif_name):
     print("Start time = {}".format(start_time_ms))
     print("End time = {}".format(end_time_ms))
     print("Total time = {}".format(end_time_ms - start_time_ms))
     i=0
     ret = True
-    frame_rate_s = cap.get(cv2.CAP_PROP_FPS)
+    frame_rate_s = video.get(cv2.CAP_PROP_FPS)
     print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {}".format(frame_rate_s))
     nb_of_frames = int((end_time_ms - start_time_ms) * frame_rate_s / 1000)
     print("nb_of_frames = {}".format(nb_of_frames))
-    cap.set(cv2.CAP_PROP_POS_MSEC, start_time_ms)
-    while(cap.isOpened() and ret):
-        ret, frame = cap.read()
+    video.set(cv2.CAP_PROP_POS_MSEC, start_time_ms)
+    while(video.isOpened() and ret):
+        ret, frame = video.read()
         frame = cv2.resize(frame, (608, 360), interpolation = cv2.INTER_AREA)
         cv2.imwrite('frame_'+str(i)+'.jpg',frame)
         i+=1
@@ -45,31 +42,49 @@ def launch_extraction(start_time_ms, end_time_ms, top_line_text, bottom_line_tex
             break
         print("Processing image n°{}".format(i))
 
-    cap.release()
+    video.release()
     cv2.destroyAllWindows()
 
     output = subprocess.check_output(['./gimp_bash.sh', "{}".format(nb_of_frames), top_line_text, bottom_line_text, gif_name])
-
-    # subprocess.call(shlex.split("./gimp_bash.sh {} {} {}".format(nb_of_frames, top_line_text, bottom_line_text)))
     print("Done")
+
+
+def update_frame(frame_label, video, time):
+    video.set(cv2.CAP_PROP_POS_MSEC, time)
+    ret, frame = video.read()
+    frame = cv2.resize(frame, (304, 180), interpolation = cv2.INTER_AREA)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB);
+    pixmap = QPixmap(304, 180)
+    imgQ = QImage(frame, 304, 180, QImage.Format_RGB888)
+    pixmap.convertFromImage(imgQ, Qt.ColorOnly)
+    frame_label.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
 
+    # Opens the Video file
+    video = cv2.VideoCapture('./test_video.mp4')
+
     window = MainWindow()
     window.show()
 
-    start_time_edit = window.ui.startTimeTextEdit
-    end_time_edit = window.ui.endTimeTextEdit
+    start_time_edit = window.ui.startTimeLineEdit
+    end_time_edit = window.ui.endTimeLineEdit
     top_line_text_edit = window.ui.topLineTextEdit
     bottom_line_text_edit = window.ui.bottomLineTextEdit
     gif_name_edit = window.ui.gifNameTextEdit
     extract_button = window.ui.extractButton
+    start_frame_label = window.ui.startFrameLabel
+    end_frame_label = window.ui.endFrameLabel
 
-    extract_button.clicked.connect(lambda: launch_extraction(int(start_time_edit.toPlainText()),
-                                                             int(end_time_edit.toPlainText()),
+    start_time_edit.returnPressed.connect(lambda : update_frame(start_frame_label, video, int(start_time_edit.text())))
+    end_time_edit.returnPressed.connect(lambda : update_frame(end_frame_label, video, int(end_time_edit.text())))
+
+    extract_button.clicked.connect(lambda: launch_extraction(video,
+                                                             int(start_time_edit.text()),
+                                                             int(end_time_edit.text()),
                                                              top_line_text_edit.toPlainText(),
                                                              bottom_line_text_edit.toPlainText(),
                                                              gif_name_edit.toPlainText()))
--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -17,42 +17,72 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(418, 397)
+        MainWindow.resize(532, 649)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.startFrameLayout = QHBoxLayout()
+        self.startFrameLayout.setObjectName(u"startFrameLayout")
+        self.startTimeLayout = QVBoxLayout()
+        self.startTimeLayout.setObjectName(u"startTimeLayout")
         self.startTimeLabel = QLabel(self.centralwidget)
         self.startTimeLabel.setObjectName(u"startTimeLabel")
         self.startTimeLabel.setMinimumSize(QSize(200, 25))
         self.startTimeLabel.setMaximumSize(QSize(200, 25))
         self.startTimeLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.startTimeLabel)
+        self.startTimeLayout.addWidget(self.startTimeLabel)
 
-        self.startTimeTextEdit = QTextEdit(self.centralwidget)
-        self.startTimeTextEdit.setObjectName(u"startTimeTextEdit")
-        self.startTimeTextEdit.setMinimumSize(QSize(200, 25))
-        self.startTimeTextEdit.setMaximumSize(QSize(200, 25))
-        self.startTimeTextEdit.setTabChangesFocus(True)
+        self.startTimeLineEdit = QLineEdit(self.centralwidget)
+        self.startTimeLineEdit.setObjectName(u"startTimeLineEdit")
 
-        self.verticalLayout.addWidget(self.startTimeTextEdit)
+        self.startTimeLayout.addWidget(self.startTimeLineEdit)
 
+
+        self.startFrameLayout.addLayout(self.startTimeLayout)
+
+        self.startFrameLabel = QLabel(self.centralwidget)
+        self.startFrameLabel.setObjectName(u"startFrameLabel")
+        self.startFrameLabel.setMinimumSize(QSize(304, 180))
+        self.startFrameLabel.setMaximumSize(QSize(304, 180))
+
+        self.startFrameLayout.addWidget(self.startFrameLabel)
+
+
+        self.verticalLayout.addLayout(self.startFrameLayout)
+
+        self.endFrameLayout = QHBoxLayout()
+        self.endFrameLayout.setObjectName(u"endFrameLayout")
+        self.endTimeLayout = QVBoxLayout()
+        self.endTimeLayout.setObjectName(u"endTimeLayout")
         self.endTimeLabel = QLabel(self.centralwidget)
         self.endTimeLabel.setObjectName(u"endTimeLabel")
         self.endTimeLabel.setMinimumSize(QSize(200, 25))
         self.endTimeLabel.setMaximumSize(QSize(200, 25))
         self.endTimeLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.endTimeLabel)
+        self.endTimeLayout.addWidget(self.endTimeLabel)
 
-        self.endTimeTextEdit = QTextEdit(self.centralwidget)
-        self.endTimeTextEdit.setObjectName(u"endTimeTextEdit")
-        self.endTimeTextEdit.setMinimumSize(QSize(200, 25))
-        self.endTimeTextEdit.setMaximumSize(QSize(200, 25))
-        self.endTimeTextEdit.setTabChangesFocus(True)
+        self.endTimeLineEdit = QLineEdit(self.centralwidget)
+        self.endTimeLineEdit.setObjectName(u"endTimeLineEdit")
+        self.endTimeLineEdit.setMinimumSize(QSize(200, 25))
+        self.endTimeLineEdit.setMaximumSize(QSize(200, 25))
 
-        self.verticalLayout.addWidget(self.endTimeTextEdit)
+        self.endTimeLayout.addWidget(self.endTimeLineEdit)
+
+
+        self.endFrameLayout.addLayout(self.endTimeLayout)
+
+        self.endFrameLabel = QLabel(self.centralwidget)
+        self.endFrameLabel.setObjectName(u"endFrameLabel")
+        self.endFrameLabel.setMinimumSize(QSize(304, 180))
+        self.endFrameLabel.setMaximumSize(QSize(304, 180))
+
+        self.endFrameLayout.addWidget(self.endFrameLabel)
+
+
+        self.verticalLayout.addLayout(self.endFrameLayout)
 
         self.topLineTextLabel = QLabel(self.centralwidget)
         self.topLineTextLabel.setObjectName(u"topLineTextLabel")
@@ -60,7 +90,7 @@ class Ui_MainWindow(object):
         self.topLineTextLabel.setMaximumSize(QSize(200, 25))
         self.topLineTextLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.topLineTextLabel)
+        self.verticalLayout.addWidget(self.topLineTextLabel, 0, Qt.AlignHCenter)
 
         self.topLineTextEdit = QTextEdit(self.centralwidget)
         self.topLineTextEdit.setObjectName(u"topLineTextEdit")
@@ -68,7 +98,7 @@ class Ui_MainWindow(object):
         self.topLineTextEdit.setMaximumSize(QSize(400, 25))
         self.topLineTextEdit.setTabChangesFocus(True)
 
-        self.verticalLayout.addWidget(self.topLineTextEdit)
+        self.verticalLayout.addWidget(self.topLineTextEdit, 0, Qt.AlignHCenter)
 
         self.bottomLineTextLabel = QLabel(self.centralwidget)
         self.bottomLineTextLabel.setObjectName(u"bottomLineTextLabel")
@@ -76,7 +106,7 @@ class Ui_MainWindow(object):
         self.bottomLineTextLabel.setMaximumSize(QSize(200, 25))
         self.bottomLineTextLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.bottomLineTextLabel)
+        self.verticalLayout.addWidget(self.bottomLineTextLabel, 0, Qt.AlignHCenter)
 
         self.bottomLineTextEdit = QTextEdit(self.centralwidget)
         self.bottomLineTextEdit.setObjectName(u"bottomLineTextEdit")
@@ -84,7 +114,7 @@ class Ui_MainWindow(object):
         self.bottomLineTextEdit.setMaximumSize(QSize(400, 25))
         self.bottomLineTextEdit.setTabChangesFocus(True)
 
-        self.verticalLayout.addWidget(self.bottomLineTextEdit)
+        self.verticalLayout.addWidget(self.bottomLineTextEdit, 0, Qt.AlignHCenter)
 
         self.gifNameLabel = QLabel(self.centralwidget)
         self.gifNameLabel.setObjectName(u"gifNameLabel")
@@ -92,7 +122,7 @@ class Ui_MainWindow(object):
         self.gifNameLabel.setMaximumSize(QSize(200, 25))
         self.gifNameLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.gifNameLabel)
+        self.verticalLayout.addWidget(self.gifNameLabel, 0, Qt.AlignHCenter)
 
         self.gifNameTextEdit = QTextEdit(self.centralwidget)
         self.gifNameTextEdit.setObjectName(u"gifNameTextEdit")
@@ -100,19 +130,19 @@ class Ui_MainWindow(object):
         self.gifNameTextEdit.setMaximumSize(QSize(200, 25))
         self.gifNameTextEdit.setTabChangesFocus(True)
 
-        self.verticalLayout.addWidget(self.gifNameTextEdit)
+        self.verticalLayout.addWidget(self.gifNameTextEdit, 0, Qt.AlignHCenter)
 
         self.extractButton = QPushButton(self.centralwidget)
         self.extractButton.setObjectName(u"extractButton")
         self.extractButton.setMinimumSize(QSize(200, 25))
         self.extractButton.setMaximumSize(QSize(200, 25))
 
-        self.verticalLayout.addWidget(self.extractButton)
+        self.verticalLayout.addWidget(self.extractButton, 0, Qt.AlignHCenter)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 418, 22))
+        self.menubar.setGeometry(QRect(0, 0, 532, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -126,7 +156,9 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.startTimeLabel.setText(QCoreApplication.translate("MainWindow", u"Start Time", None))
+        self.startFrameLabel.setText(QCoreApplication.translate("MainWindow", u"Start Frame", None))
         self.endTimeLabel.setText(QCoreApplication.translate("MainWindow", u"End Time", None))
+        self.endFrameLabel.setText(QCoreApplication.translate("MainWindow", u"EndFrame", None))
         self.topLineTextLabel.setText(QCoreApplication.translate("MainWindow", u"Top Line Text", None))
         self.bottomLineTextLabel.setText(QCoreApplication.translate("MainWindow", u"Bottom Line Text", None))
         self.gifNameLabel.setText(QCoreApplication.translate("MainWindow", u"Gif name", None))
